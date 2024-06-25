d2aa40870ec3f3d8de2a478f5e0b86286bc7ef7b
Mario <mario@ipedis.com>
2024-06-25 11:06:19 +0400

    🧪  update test

--- packages/toggle/src/components/ip-toggle/toggle.spec.ts
+++ packages/toggle/src/components/ip-toggle/toggle.spec.ts
@@ -3,41 +3,52 @@ import { ToggleButton } from "./toggle";
 
 describe("ip-toggle", () => {
   it("renders", async () => {
-    const { root } = await newSpecPage({
+    const page = await newSpecPage({
       components: [ToggleButton],
       html: "<ip-toggle></ip-toggle>",
     });
-    expect(root).toEqualHtml(`
+    expect(page.root).toEqualHtml(`
       <ip-toggle>
         <mock:shadow-root>
+        <div class="switch-container">
+          <slot name="switch-label"></slot>
            <div class="switch-button">
              <input aria-checked="false" class="switch-checkbox" role="switch" />
                <span class="switch-icon"></span>
+          <slot name="helper-text"></slot>
+        </div>
         </mock:shadow-root>
       </ip-toggle>
     `);
   });
 
   it("renders with values", async () => {
-    const { root } = await newSpecPage({
+    const page = await newSpecPage({
       components: [ToggleButton],
       html: `<ip-toggle active-label="Oui" inactive-label="Non"></ip-toggle>`,
     });
-    expect(root).toEqualHtml(`
-      <ip-toggle active-label="Oui" inactive-label="Non">
-        <mock:shadow-root>
-         <div class="switch-button">
-           <input aria-checked="false" class="switch-checkbox" role="switch" />
-             <span class="switch-icon"></span>
-             <span class="label">
-               <p class="inactive-label">
-                 Non
-               </p>
-             </span>
-
-          </div>
-        </mock:shadow-root>
-      </ip-toggle>
+    expect(page.root).toEqualHtml(`
+    <ip-toggle active-label="Oui" inactive-label="Non">
+    <mock:shadow-root>
+      <div class="switch-container">
+        <slot name="switch-label"></slot>
+        <div class="switch-button">
+          <input type="checkbox" aria-checked="false" class="switch-checkbox" role="switch" />
+          <span class="switch-icon">
+            <span class="label">
+              <p class="active-label">
+                Oui
+              </p>
+              <p class="inactive-label">
+                Non
+              </p>
+            </span>
+          </span>
+        </div>
+        <slot name="helper-text"></slot>
+      </div>
+    </mock:shadow-root>
+  </ip-toggle>
     `);
   });
 });
