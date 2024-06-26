9fb23bcb86ac0bc4da90c1b14ac6683279327065
patrickIpedis <patrick@ipedis.com>
2024-06-26 10:55:43 +0400

    ✨ feat: create checkbox component

--- packages/checkbox/src/components.d.ts
+++ packages/checkbox/src/components.d.ts
@@ -16,6 +16,16 @@ export namespace Components {
         "legend": string;
         "options": string;
     }
+    interface IpEmail {
+        "errorMessage": string;
+        "inputLabel": string;
+        "invalid": boolean;
+    }
+    interface IpPassword {
+        "errorMessage": string;
+        "forgotPasswordLink": string;
+        "invalid": boolean;
+    }
 }
 export interface IpCheckboxListCustomEvent<T> extends CustomEvent<T> {
     detail: T;
@@ -45,9 +55,23 @@ declare global {
         prototype: HTMLIpCheckboxListElement;
         new (): HTMLIpCheckboxListElement;
     };
+    interface HTMLIpEmailElement extends Components.IpEmail, HTMLStencilElement {
+    }
+    var HTMLIpEmailElement: {
+        prototype: HTMLIpEmailElement;
+        new (): HTMLIpEmailElement;
+    };
+    interface HTMLIpPasswordElement extends Components.IpPassword, HTMLStencilElement {
+    }
+    var HTMLIpPasswordElement: {
+        prototype: HTMLIpPasswordElement;
+        new (): HTMLIpPasswordElement;
+    };
     interface HTMLElementTagNameMap {
         "ip-checkbox": HTMLIpCheckboxElement;
         "ip-checkbox-list": HTMLIpCheckboxListElement;
+        "ip-email": HTMLIpEmailElement;
+        "ip-password": HTMLIpPasswordElement;
     }
 }
 declare namespace LocalJSX {
@@ -62,9 +86,21 @@ declare namespace LocalJSX {
         "onSelectionChanged"?: (event: IpCheckboxListCustomEvent<string[]>) => void;
         "options"?: string;
     }
+    interface IpEmail {
+        "errorMessage"?: string;
+        "inputLabel"?: string;
+        "invalid"?: boolean;
+    }
+    interface IpPassword {
+        "errorMessage"?: string;
+        "forgotPasswordLink"?: string;
+        "invalid"?: boolean;
+    }
     interface IntrinsicElements {
         "ip-checkbox": IpCheckbox;
         "ip-checkbox-list": IpCheckboxList;
+        "ip-email": IpEmail;
+        "ip-password": IpPassword;
     }
 }
 export { LocalJSX as JSX };
@@ -73,6 +109,8 @@ declare module "@stencil/core" {
         interface IntrinsicElements {
             "ip-checkbox": LocalJSX.IpCheckbox & JSXBase.HTMLAttributes<HTMLIpCheckboxElement>;
             "ip-checkbox-list": LocalJSX.IpCheckboxList & JSXBase.HTMLAttributes<HTMLIpCheckboxListElement>;
+            "ip-email": LocalJSX.IpEmail & JSXBase.HTMLAttributes<HTMLIpEmailElement>;
+            "ip-password": LocalJSX.IpPassword & JSXBase.HTMLAttributes<HTMLIpPasswordElement>;
         }
     }
 }
