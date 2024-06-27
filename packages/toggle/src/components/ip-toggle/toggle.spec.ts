eac3b2285f577a0446862bc0f7fcc881fb09b6cc
Yanis <yanis.ghidouche@gmail.com>
2024-06-27 10:09:15 +0400

    feat: toggle - fix tu

--- packages/toggle/src/components/ip-toggle/toggle.spec.ts
+++ packages/toggle/src/components/ip-toggle/toggle.spec.ts
@@ -13,9 +13,10 @@ describe("ip-toggle", () => {
         <div class="switch-container">
           <slot name="switch-label"></slot>
            <div class="switch-button">
-             <input aria-checked="false" class="switch-checkbox" role="switch" />
-               <span class="switch-icon"></span>
-          <slot name="helper-text"></slot>
+               <input aria-checked="false" class="medium switch-checkbox" role="switch" type="checkbox">
+               <span class="medium switch-icon"></span>
+          </div>
+           <slot name="helper-text"></slot>
         </div>
         </mock:shadow-root>
       </ip-toggle>
@@ -29,25 +30,22 @@ describe("ip-toggle", () => {
     });
     expect(page.root).toEqualHtml(`
     <ip-toggle active-label="Oui" inactive-label="Non">
-    <mock:shadow-root>
-      <div class="switch-container">
-        <slot name="switch-label"></slot>
-        <div class="switch-button">
-          <input type="checkbox" aria-checked="false" class="switch-checkbox" role="switch" />
-          <span class="switch-icon">
-            <span class="label">
-              <p class="active-label">
-                Oui
-              </p>
-              <p class="inactive-label">
-                Non
-              </p>
-            </span>
-          </span>
-        </div>
-        <slot name="helper-text"></slot>
-      </div>
-    </mock:shadow-root>
+        <mock:shadow-root>
+          <div class="switch-container">
+            <slot name="switch-label"></slot>
+            <div class="switch-button">
+              <input aria-checked="false" class="medium switch-checkbox" role="switch" type="checkbox">
+              <span class="medium switch-icon">
+                <span class="label">
+                  <p class="inactive-label medium">
+                    Non
+                  </p>
+                </span>
+              </span>
+            </div>
+            <slot name="helper-text"></slot>
+          </div>
+        </mock:shadow-root>
   </ip-toggle>
     `);
   });
