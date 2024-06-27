3dc1a4f79ea3b3a88781876a73d2e61b8a0e8ee1
TOLOJANAHARY Patrick André <146705731+patrickIpedis@users.noreply.github.com>
2024-06-27 13:47:44 +0400

    Migration of WC-ip-checkbox to mono repo  (#4)
    * ✨ feat: create checkbox component
    
    * ✨ feat: migrate ip-checkbox
    
    * ✅ test(ip-checkbox): add runnable tests of ip-checkbox
    
    * ✅ test(ip-checkbox-list): add runnable tests of ip-checkbox-list
    
    * 🔧 generated test config for checkbox
    
    * feat: checkbox - fix tu
    
    * feat: checkbox - fix config - disable pending test
    
    ---------
    
    Co-authored-by: Yanis <yanis.ghidouche@gmail.com>

---
+++ packages/checkbox/src/components/ip-checkbox-list/ip-checkbox-list.spec.ts
@@ -0,0 +1,107 @@
+import { newSpecPage } from "@stencil/core/testing";
+import { IpCheckboxList } from "./ip-checkbox-list";
+
+describe("ip-checkbox-list", () => {
+  it("renders with errors", async () => {
+    /* global global */
+    const mockedConsole = jest
+      .spyOn(global.console, "error")
+      .mockImplementation();
+
+    const { root } = await newSpecPage({
+      components: [IpCheckboxList],
+      html: `<ip-checkbox-list options="{}"></ip-checkbox-list>`,
+    });
+
+    expect(console.error).toHaveBeenCalledTimes(1);
+
+    expect(root).toEqualHtml(`
+      <ip-checkbox-list options="{}">
+         <mock:shadow-root>
+           <div class="checkbox-list">
+             <fieldset class="checkbox-content"></fieldset>
+           </div>
+         </mock:shadow-root>
+      </ip-checkbox-list>
+    `);
+
+    mockedConsole.mockRestore();
+  });
+
+  it("renders", async () => {
+    const { root } = await newSpecPage({
+      components: [IpCheckboxList],
+      html: `<ip-checkbox-list></ip-checkbox-list>`,
+    });
+
+    expect(root).toEqualHtml(`
+      <ip-checkbox-list>
+         <mock:shadow-root>
+           <div class="checkbox-list">
+             <fieldset class="checkbox-content"></fieldset>
+           </div>
+         </mock:shadow-root>
+      </ip-checkbox-list>
+    `);
+  });
+
+  it("renders with options", async () => {
+    const { root } = await newSpecPage({
+      components: [IpCheckboxList],
+      html: `<ip-checkbox-list
+                legend="Preferences:"
+                options='[
+                    {"id": "1", "label": "Option 1"},
+                    {"id": "2", "label": "Option 2"},
+                    {"id": "3", "label": "Option 3"}
+                ]'
+                >
+             </ip-checkbox-list>"`,
+    });
+    expect(root).toEqualHtml(`
+      <ip-checkbox-list legend="Preferences:" options="[
+                    {&quot;id&quot;: &quot;1&quot;, &quot;label&quot;: &quot;Option 1&quot;},
+                    {&quot;id&quot;: &quot;2&quot;, &quot;label&quot;: &quot;Option 2&quot;},
+                    {&quot;id&quot;: &quot;3&quot;, &quot;label&quot;: &quot;Option 3&quot;}
+                ]">
+        <mock:shadow-root>
+            <div class="checkbox-list">
+                <fieldset class="checkbox-content">
+                    <legend class="legend">Preferences:</legend>
+                    <div>
+                        <input
+                            class="checkbox-input"
+                            type="checkbox"
+                            id="1"
+                            aria-checked="false"
+                            role="checkbox"
+                        />
+                        <label class="checkbox-label" htmlfor="1">Option 1</label>
+                    </div>
+                    <div>
+                        <input
+                            class="checkbox-input"
+                            type="checkbox"
+                            id="2"
+                            aria-checked="false"
+                            role="checkbox"
+                        />
+                        <label class="checkbox-label" htmlfor="2">Option 2</label>
+                    </div>
+                    <div>
+                    <input
+                        class="checkbox-input"
+                        type="checkbox"
+                        id="3"
+                        aria-checked="false"
+                        role="checkbox"
+                    />
+                        <label class="checkbox-label" htmlfor="3">Option 3</label>
+                    </div>
+                </fieldset>
+            </div>
+        </mock:shadow-root>
+      </ip-checkbox-list>
+    `);
+  });
+});
---
+++ packages/checkbox/src/components/ip-checkbox/ip-checkbox.spec.ts
@@ -0,0 +1,46 @@
+import { newSpecPage } from "@stencil/core/testing";
+import { IpCheckbox } from "./ip-checkbox";
+
+describe("ip-checkbox", () => {
+  it("renders", async () => {
+    const { root } = await newSpecPage({
+      components: [IpCheckbox],
+      html: "<ip-checkbox></ip-checkbox>",
+    });
+    expect(root).toEqualHtml(`
+      <ip-checkbox>
+        <mock:shadow-root>
+          <div class="checkbox-content">
+            <input 
+                class="checkbox-input"
+                type="checkbox">
+                <label class="checkbox-label">
+                    <slot></slot>
+                </label>
+          </div>
+        </mock:shadow-root>
+      </ip-checkbox>
+    `);
+  });
+
+  //This test will fail because of the slot being hard to test
+  it("renders with values", async () => {
+    const { root } = await newSpecPage({
+      components: [IpCheckbox],
+      html: `  <ip-checkbox default-checked="true" identifier="check me">Check me !</ip-checkbox>`,
+    });
+    expect(root).toEqualHtml(`
+     <ip-checkbox default-checked="true" identifier="check me">
+       <mock:shadow-root>
+         <div class="checkbox-content">
+           <input checked="" class="checkbox-input" id="check me" type="checkbox">
+           <label class="checkbox-label" htmlfor="check me">
+             <slot></slot>
+           </label>
+         </div>
+       </mock:shadow-root>
+       Check me !
+     </ip-checkbox>
+    `);
+  });
+});
