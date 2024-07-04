82542764329ec54b58dff22aec8337500c619340
Mario <mario@ipedis.com>
2024-07-04 12:25:47 +0400

    🚧  lint

--- packages/dropdown/src/components/ip-dropdown/dropdown.tsx
+++ packages/dropdown/src/components/ip-dropdown/dropdown.tsx
@@ -81,71 +81,71 @@ export class Dropdown {
 
   render() {
     return (
-      <div class='dropdown'>
-        <div class='dropdown-title' aria-label={this.dropdownTitle}>
+      <div class="dropdown">
+        <div class="dropdown-title" aria-label={this.dropdownTitle}>
           {this.dropdownTitle}
         </div>
         <div
-          role='combobox'
-          aria-labelledby='dropdown-title'
-          class='dropdown-content'
-          tabindex='0'
+          role="combobox"
+          aria-labelledby="dropdown-title"
+          class="dropdown-content"
+          tabindex="0"
           onClick={() => this.toggleDropdown()}
           onKeyDown={(event) => this.handleKeyDown(event)}
-          aria-expanded={this.isOpen ? 'true' : 'false'}
+          aria-expanded={this.isOpen ? "true" : "false"}
         >
-          <span class='dropdown-head' role='button' aria-haspopup='listbox'>
+          <span class="dropdown-head" role="button" aria-haspopup="listbox">
             {this.selectedItem || this.placeholder}
           </span>
-          <span class='dropdown-arrow' aria-hidden='true'>
+          <span class="dropdown-arrow" aria-hidden="true">
             <svg
-              width='18'
-              height='18'
-              viewBox='0 0 18 18'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <g id='Icon/CaretDown'>
+              <g id="Icon/CaretDown">
                 <path
-                  id='Vector'
-                  d='M14.625 6.75L9 12.375L3.375 6.75'
-                  stroke='#2749A3'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
+                  id="Vector"
+                  d="M14.625 6.75L9 12.375L3.375 6.75"
+                  stroke="#2749A3"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
                 />
               </g>
             </svg>
           </span>
         </div>
         {this.isOpen && (
-          <ul role='listbox' class='dropdown-list'>
+          <ul role="listbox" class="dropdown-list">
             {this.items.map((item, index) => (
               <li
                 key={index}
-                role='option'
-                aria-selected={item === this.selectedItem ? 'true' : 'false'}
-                class={item === this.selectedItem ? 'selected' : ''}
+                role="option"
+                aria-selected={item === this.selectedItem ? "true" : "false"}
+                class={item === this.selectedItem ? "selected" : ""}
                 onClick={() => this.selectItem(item)}
                 onKeyDown={(event) => this.keySelectItem(event, item)}
-                tabindex='0'
+                tabindex="0"
               >
                 {item}
                 {item === this.selectedItem && (
-                  <span class='selected-icon'>
+                  <span class="selected-icon">
                     <svg
-                      width='20'
-                      height='20'
-                      viewBox='0 0 20 20'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <g id='Icon/Check'>
+                      <g id="Icon/Check">
                         <path
-                          id='Vector'
-                          d='M16.875 5.625L8.125 14.375L3.75 10'
-                          stroke='#2749A3'
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
+                          id="Vector"
+                          d="M16.875 5.625L8.125 14.375L3.75 10"
+                          stroke="#2749A3"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
                         />
                       </g>
                     </svg>
