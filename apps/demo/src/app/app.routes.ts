d710ae0884dc4de6c5c14af5a40be153208da36e
Mario <mario@ipedis.com>
2024-07-22 12:55:55 +0400

    🚧 integrate dropdown

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TooltipComponent } from './pages/tooltip/tooltip.component';
-import { NgModule } from '@angular/core';
+import { DropdownComponent } from './pages/dropdown/dropdown.component';
 
 export const appRoutes: Route[] = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
-    {path:'tooltip', component: TooltipComponent},
+    { path: 'tooltip', component: TooltipComponent },
+    { path: 'dropdown', component: DropdownComponent },
     { path: '**', redirectTo: '' },
 ];
