b58eb690ff157af7443e17ee9f94a23d2603dc3c
Yanis <yanis.ghidouche@gmail.com>
2024-07-24 15:38:35 +0400

    feat(demo): resolve major conflict

--- packages/login-angular/src/directives/proxies.ts
+++ packages/login-angular/src/directives/proxies.ts
@@ -1,32 +1,36 @@
 /* tslint:disable */
 /* auto-generated angular directive proxies */
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, NgZone } from '@angular/core';
 
-import { ProxyCmp } from './angular-component-lib/utils';
+import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
 
 import { Components } from 'login';
 
 
 @ProxyCmp({
-  inputs: ['errorMessage', 'inputLabel', 'invalid']
+  inputs: ['errorMessage', 'inputLabel', 'invalid', 'required']
 })
 @Component({
   selector: 'ip-email',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: '<ng-content></ng-content>',
   // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
-  inputs: ['errorMessage', 'inputLabel', 'invalid'],
+  inputs: ['errorMessage', 'inputLabel', 'invalid', 'required'],
 })
 export class IpEmail {
   protected el: HTMLElement;
   constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
     c.detach();
     this.el = r.nativeElement;
+    proxyOutputs(this, this.el, ['inputChange']);
   }
 }
 
 
-export declare interface IpEmail extends Components.IpEmail {}
+export declare interface IpEmail extends Components.IpEmail {
+
+  inputChange: EventEmitter<CustomEvent<string>>;
+}
 
 
 @ProxyCmp({
@@ -44,10 +48,14 @@ export class IpPassword {
   constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
     c.detach();
     this.el = r.nativeElement;
+    proxyOutputs(this, this.el, ['passwordChange']);
   }
 }
 
 
-export declare interface IpPassword extends Components.IpPassword {}
+export declare interface IpPassword extends Components.IpPassword {
+
+  passwordChange: EventEmitter<CustomEvent<string>>;
+}
 
 
