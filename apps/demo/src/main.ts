ee3b364d7d8241d01978d0fe493221f2095afacf
Mario <mario@ipedis.com>
2024-07-24 09:51:56 +0400

    🐛 fix lint and remove console log

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { defineCustomElements as tooltipElements } from '../../../dist/packages/tooltip/loader';
-import { defineCustomElements as toggleElements } from '../../../dist/packages/toggle/loader';
-import { defineCustomElements as checkboxElements } from '../../../dist/packages/checkbox/loader';
-import { defineCustomElements as loginElements } from '../../../dist/packages/login/loader';
-import { defineCustomElements as dropdownElements } from '../../../dist/packages/dropdown/loader';
-import { defineCustomElements as radioElements } from '../../../dist/packages/radio/loader';
+import { defineCustomElements as tooltipElements } from 'dist/packages/tooltip/loader';
+import { defineCustomElements as toggleElements } from 'dist/packages/toggle/loader';
+import { defineCustomElements as checkboxElements } from 'dist/packages/checkbox/loader';
+import { defineCustomElements as loginElements } from 'dist/packages/login/loader';
+import { defineCustomElements as dropdownElements } from 'dist/packages/dropdown/loader';
+import { defineCustomElements as radioElements } from 'dist/packages/radio/loader';
 
 tooltipElements(window);
 toggleElements(window);
