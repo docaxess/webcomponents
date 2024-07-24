24bad9608417eed23065075a6846ef76e1f4c652
Mario <mario@ipedis.com>
2024-07-24 14:27:44 +0400

    ✨ add radio-button component

--- apps/demo/src/app/pages/radio-button/radio-button.component.ts
+++ apps/demo/src/app/pages/radio-button/radio-button.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CodeSnippetComponent } from '../code-snippet/code-snippet.component';
 
 @Component({
   selector: 'app-radio-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CodeSnippetComponent],
   templateUrl: './radio-button.component.html',
   styleUrl: './radio-button.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class RadioButtonComponent {}
+export class RadioButtonComponent {
+  codeSnippet = `
+      <ip-radio
+        id="radio"
+        legend="What is your gender"
+        default-option-id="Female"
+        options='[
+      {"id": "Male", "label": "Male"}, 
+      {"id": "Female", "label": "Female"},
+      {"id": "Other", "label": "Other", "disabled": true}
+
+      ]'
+      ></ip-radio>
+  `
+}
