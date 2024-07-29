a6bd880eb40ef21dae2d055164af25fac4e39f2f
Mario <mario@ipedis.com>
2024-07-29 17:52:45 +0400

    🚧 tab-panel integration

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -1,22 +1,66 @@
 import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
-    { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
- },
-    { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
- },
-    { path: 'tooltip', loadComponent: () => import('./pages/tooltip/tooltip.component').then(m => m.TooltipComponent)
-    },
-    { path: 'dropdown', loadComponent: () => import('./pages/dropdown/dropdown.component').then(m => m.DropdownComponent)
-    },
-    { path: 'toggle', loadComponent: () => import('./pages/toggle/toggle.component').then(m => m.ToggleComponent)
-    },
-    { path: 'radio-button', loadComponent: () => import('./pages/radio-button/radio-button.component').then(m => m.RadioButtonComponent)
-    },
-    { path: 'checkbox', loadComponent: () => import('./pages/checkbox/checkbox.component').then(m => m.CheckboxComponent)
-    },
-    { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
-    },
-    { path: 'pagination', loadComponent: () => import('./pages/pagination/pagination.component').then(m => m.PaginationComponent)
-    },
-    { path: '**', redirectTo: '' },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'tooltip',
+    loadComponent: () =>
+      import('./pages/tooltip/tooltip.component').then(
+        (m) => m.TooltipComponent,
+      ),
+  },
+  {
+    path: 'dropdown',
+    loadComponent: () =>
+      import('./pages/dropdown/dropdown.component').then(
+        (m) => m.DropdownComponent,
+      ),
+  },
+  {
+    path: 'toggle',
+    loadComponent: () =>
+      import('./pages/toggle/toggle.component').then((m) => m.ToggleComponent),
+  },
+  {
+    path: 'radio-button',
+    loadComponent: () =>
+      import('./pages/radio-button/radio-button.component').then(
+        (m) => m.RadioButtonComponent,
+      ),
+  },
+  {
+    path: 'checkbox',
+    loadComponent: () =>
+      import('./pages/checkbox/checkbox.component').then(
+        (m) => m.CheckboxComponent,
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'pagination',
+    loadComponent: () =>
+      import('./pages/pagination/pagination.component').then(
+        (m) => m.PaginationComponent,
+      ),
+  },
+  {
+    path: 'tab-panel',
+    loadComponent: () =>
+      import('./pages/tab-panel/tab-panel.component').then(
+        (m) => m.TabPanelComponent,
+      ),
+  },
+  { path: '**', redirectTo: '' },
 ];
