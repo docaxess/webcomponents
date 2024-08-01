b4a4a2cfbc087bf2d4df515e6a751fedb8aa41d9
Mario <mario@ipedis.com>
2024-08-01 17:23:59 +0400

    🚧 WIP design demo

--- apps/demo/src/app/pages/tooltip/tooltip.component.ts
+++ apps/demo/src/app/pages/tooltip/tooltip.component.ts
@@ -1,49 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CardComponent } from '../../features/card/card.component';
-import { Tooltip1Component } from './tooltip1/tooltip1.component';
-import { Tooltip2Component } from './tooltip2/tooltip2.component';
-import { Tooltip3Component } from './tooltip3/tooltip3.component';
-import { ModalComponent } from '../../features/modal/modal.component';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+  PLATFORM_ID,
+} from '@angular/core';
+import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { CodeSnippetComponent } from '../code-snippet/code-snippet.component';
+import { defineCustomElements as tooltipElements } from '@ipedis/tooltip/loader';
+import { WrapperComponent } from '../wrapper/wrapper.component';
 
 @Component({
   selector: 'app-demo-tooltip',
   standalone: true,
-  imports: [CommonModule, CardComponent, ModalComponent],
+  imports: [CommonModule, CodeSnippetComponent, WrapperComponent],
   templateUrl: './tooltip.component.html',
   styleUrl: './tooltip.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TooltipComponent {
-  modalVisible = false;
-  modalTitle = '';
-  selectedContentComponent: any;
-  cards = [
-    {
-      title: 'Clickable tooltip',
-      imageUrl: 'assets/images/tab-img-1.png',
-      contentComponent: Tooltip1Component,
-    },
-    {
-      title: 'Hover tooltip',
-      imageUrl: 'assets/images/tab-img-1.png',
-      contentComponent: Tooltip2Component,
-    },
-    {
-      title: 'Tooltip with button',
-      imageUrl: 'assets/images/tab-img-1.png',
-      contentComponent: Tooltip3Component,
-    },
-  ];
-
-  openModal(contentComponent: any, title: string) {
-    this.modalVisible = true;
-    this.modalTitle = title;
-    this.selectedContentComponent = contentComponent;
-  }
-
-  closeModal() {
-    this.modalVisible = false;
-    this.selectedContentComponent = null;
+  constructor() {
+    if (isPlatformBrowser(inject(PLATFORM_ID)) && tooltipElements) {
+      tooltipElements(inject(DOCUMENT).defaultView as Window);
+    }
   }
 }
