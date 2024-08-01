f17e583baf1f603d3778a505e0d3c723d9986953
Mario <mario@ipedis.com>
2024-08-01 17:23:59 +0400

    🚧 WIP design demo

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
 import { AsideComponent } from './pages/aside/aside.component';
 import { CodeSnippetComponent } from './pages/code-snippet/code-snippet.component';
+import { filter } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [ RouterOutlet, HeaderComponent , AsideComponent, CodeSnippetComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AsideComponent,
+    CodeSnippetComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'demo';
