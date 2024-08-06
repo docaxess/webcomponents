ff111a27b52f43f81a5e277fdfa506f51f0ab1b2
Mario <mario@ipedis.com>
2024-08-06 17:51:23 +0400

    🚧 wip , integration design

--- apps/demo/src/app/pages/radio-button/radio-button.component.ts
+++ apps/demo/src/app/pages/radio-button/radio-button.component.ts
@@ -1,21 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
+import { Radio1ButtonComponent } from './radio1/radio1.component';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-radio-button',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, ModalComponent],
   templateUrl: './radio-button.component.html',
   styleUrl: './radio-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RadioButtonComponent {
+  modalVisible = false;
+  modalTitle = '';
+  selectedContentComponent: any;
   cards = [
     {
       title: 'Radio button 1',
-      link: '/radio',
       imageUrl: 'assets/images/tab-img-1.png',
+      contentComponent: Radio1ButtonComponent,
     },
   ];
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  openModal(contentComponent: any, title: string) {
+    this.modalVisible = true;
+    this.modalTitle = title;
+    this.selectedContentComponent = contentComponent;
+    this.cdRef.markForCheck();
+  }
+
+  closeModal() {
+    this.modalVisible = false;
+    this.selectedContentComponent = null;
+    this.cdRef.markForCheck();
+  }
 }
