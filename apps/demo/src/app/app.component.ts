6a16ca5c8b0755ed6388346221fd77ae4478df82
patrickIpedis <patrick@ipedis.com>
2024-08-23 11:42:44 +0400

    🎨 fix(skip-link): don't use smooth animation

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './features/header/header.component';
 import { AsideComponent } from './features/aside/aside.component';
 import { FooterComponent } from './features/footer/footer.component';
+import { SkipLinkComponent } from './components/skip-link/skip-link.component';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AsideComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent,
+    SkipLinkComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
