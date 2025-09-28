a2d6da2de62ddde1bef7650d7f1da84c196a9c8b
Yanis <yanis.ghidouche@gmail.com>
2025-09-28 15:16:22 +0400

    feat(upgrade): update dependencies and refactor component imports

--- apps/demo/src/app/features/header/header.component.ts
+++ apps/demo/src/app/features/header/header.component.ts
@@ -1,19 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  constructor(
-    private router: Router,
-  ) { }
+  private router = inject(Router);
+
 
   goToTooltip() {
     this.router.navigate(['/tooltip']);
