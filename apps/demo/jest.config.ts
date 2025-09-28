01a211cffb2686b5cc2f662c4e6604b8767734ee
Yanis <yanis.ghidouche@gmail.com>
2025-09-28 20:00:44 +0400

    chore: update Dockerfile and CI configuration to use npm ci for consistent dependency installation

--- apps/demo/jest.config.ts
+++ apps/demo/jest.config.ts
@@ -4,6 +4,7 @@ export default {
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../coverage/demo',
+  testPathIgnorePatterns: ['<rootDir>/e2e/'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
